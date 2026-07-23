9ba91a2edbe0d605cf0c1b1cdffa5f1d7a3114e7
Yan Araujo Santos da Campo <67839882+nikito060195@users.noreply.github.com>
2026-07-23 11:42:52 -0300

    Create ex_lammps_parallel_openmp_mpi.sh

---
+++ ex_lammps_parallel_openmp_mpi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=lammps_hibrido
+#SBATCH --partition=pesquisa
+#SBATCH --nodes=1
+#SBATCH --ntasks=4                  # 4 processos MPI totais (2 para Sim A + 2 para Sim B)
+#SBATCH --cpus-per-task=4           # 4 threads OpenMP por processo MPI
+#SBATCH --time=30:00:00
+#SBATCH --output=lammps_%j.log
+
+cd "$SLURM_SUBMIT_DIR"
+echo "Iniciando simulacoes paralelas de LAMMPS (MPI + OpenMP)..."
+
+# --- SIMULACAO A (2 tarefas MPI x 4 threads OpenMP = 8 threads) ---
+srun --exclusive -N1 -n2 --cpus-per-task=4 \
+apptainer exec /home/nome_usuario/meus_ambientes/lammps_cpu.sif \
+lmp -sf omp -pk omp 4 -in in.sistemaA > log_simulacaoA.txt 2>&1 &
+
+# --- SIMULACAO B (2 tarefas MPI x 4 threads OpenMP = 8 threads) ---
+srun --exclusive -N1 -n2 --cpus-per-task=4 \
+apptainer exec /home/nome_usuario/meus_ambientes/lammps_cpu.sif \
+lmp -sf omp -pk omp 4 -in in.sistemaB > log_simulacaoB.txt 2>&1 &
+
+# Impede que o script feche antes das simulacoes terminarem
+wait
+
+echo "Simulacoes do LAMMPS concluidas."
