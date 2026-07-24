e46f30507638b9f94d1c4c9b5d7716a7b85afff7
Yan Araujo Santos da Campo <67839882+nikito060195@users.noreply.github.com>
2026-07-24 18:14:17 -0300

    Update ex_lammps_parallel_openmp_mpi.sh

--- ex_lammps_parallel_openmp_mpi.sh
+++ ex_lammps_parallel_openmp_mpi.sh
@@ -10,17 +10,18 @@
 cd "$SLURM_SUBMIT_DIR"
 echo "Iniciando simulacoes paralelas de LAMMPS (MPI + OpenMP)..."
 
-# --- SIMULACAO A (2 tarefas MPI x 4 threads OpenMP = 8 threads) ---
-srun --exclusive -N1 -n2 --cpus-per-task=4 \
-apptainer exec /home/nome_usuario/meus_ambientes/lammps_cpu.sif \
-lmp -sf omp -pk omp 4 -in in.sistemaA > log_simulacaoA.txt 2>&1 &
+# Repassa as variaveis alocadas pelo Slurm para o ambiente
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# --- SIMULACAO B (2 tarefas MPI x 4 threads OpenMP = 8 threads) ---
-srun --exclusive -N1 -n2 --cpus-per-task=4 \
-apptainer exec /home/nome_usuario/meus_ambientes/lammps_cpu.sif \
-lmp -sf omp -pk omp 4 -in in.sistemaB > log_simulacaoB.txt 2>&1 &
+# --- SIMULACAO A (2 tarefas MPI x 4 threads OpenMP = 8 núcleos isolados) ---
+apptainer exec /home/nome_de_usuario/meus_ambientes/lammps_cpu.sif \
+mpirun --bind-to none -np 2 lmp -sf omp -pk omp $OMP_NUM_THREADS -in in.sistemaA > log_simulacaoA.txt 2>&1 &
+
+# --- SIMULACAO B (2 tarefas MPI x 4 threads OpenMP = 8 núcleos isolados) ---
+apptainer exec /home/nome_de_usuario/meus_ambientes/lammps_cpu.sif \
+mpirun --bind-to none -np 2 lmp -sf omp -pk omp $OMP_NUM_THREADS -in in.sistemaB > log_simulacaoB.txt 2>&1 &
 
 # Impede que o script feche antes das simulacoes terminarem
 wait
 
-echo "Simulacoes do LAMMPS concluidas."
+echo "Simulacoes do LAMMPS concluidas com sucesso!"
